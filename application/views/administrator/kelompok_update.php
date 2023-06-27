ccc041d5dc6bc0b8a5b8986784d8bb3749c4b6f4
Fellaattaqi <136142290+Fellaattaqi@users.noreply.github.com>
2023-06-27 12:33:20 +0700

    update

--- application/views/administrator/kelompok_update.php
+++ application/views/administrator/kelompok_update.php
@@ -9,6 +9,7 @@
     <?php foreach($kelompok as $klp) : ?>
 
         <form method="post" action="<?php echo base_url('administrator/kelompok/update_aksi') ;?>">
+        <?php echo $this->session->flashdata('pesan'); ?>
             <div class="form-group">
                 <label>Kode Kelompok</label>
                 <input type="hidden" name="id_kelompok" value="<?php echo $klp->id_kelompok ?>">
